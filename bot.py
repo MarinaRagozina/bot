6f544b81046eedd0d59c0c61da1bce8e1ea86ec4
Marina <ragozina_2000@mail.ru>
2023-04-20 00:53:46 +0300

    Update files

--- bot.py
+++ bot.py
@@ -1,27 +1,53 @@
-from functions import *
+import requests
+from fuzzywuzzy import fuzz
 
-hello_message = """Привет! Предлагаю поиграть в викторину на английском языку.
-Отвечай на вопросы и зарабатывай очки. Количество вопросов ты определяешь самостоятельно."""
-print(hello_message)
-question_amount_range = range(1, 101)
-for i in range(2):
-    user_input = input("Введи число от 1 до 100: ")
-    # проверка, что инпут - целое число в промежутке от 1 до 100 (ограничение API)
-    if user_input.isdigit() and int(user_input) in question_amount_range:
-        question_amount = int(user_input)
-        break
-    else:
-        question_amount = None
-        print("Упс, неверный формат ввода.")  # Добавить что попробуй еще раз
+class Bot:
+    def __init__(self):
+        self.question_amount_range = range(1, 101)
+        self.total_score = 0
+        self.url = "http://jservice.io/api/random"
 
-total_score = 0
-response = make_api_call(question_amount)
-if response:
-    # вывод вопросов и подсчет числа правильных ответов
-    for i, element in enumerate(response.json()):
-        print(element["answer"])  # УБРАТЬ ПЕРЕД ОТПРАВКОЙ
-        print("\nВопрос {}: {}".format(i+1, element["question"]))
-        total_score += score_user_answer(element["answer"].lower())
-    print("Спасибо за игру! Твой результат: {}".format(total_score))
-else:
-    print("База вопросов что-то барахлит, не удается к ней подключиться. Попробуй еще раз позже.")
+    def run(self):
+        user_input = input("Введи число от 1 до 100: ")
+        if user_input.isdigit() and int(user_input) in self.question_amount_range:
+            question_amount = int(user_input)
+        else:
+            print("Упс, неверный формат ввода. Попробуй еще раз.")
+            self.run()
+            return
+        response = self.make_api_call(question_amount)
+        if response:
+            for i, element in enumerate(response.json()):
+                print("\nВопрос {}: {}".format(i+1, element["question"]))
+                self.total_score += self.score_user_answer(element["answer"].lower())
+            print("Спасибо за игру! Количество правильных ответов: {}".format(self.total_score))
+        else:
+            print("База вопросов что-то барахлит, не удается к ней подключиться. Попробуй еще раз позже.")
+
+    def make_api_call(self, question_amount):
+        if not question_amount:
+            return None
+        try:
+            response = requests.get(self.url, params={"count": question_amount})
+            response.raise_for_status()
+            return response
+        except:
+            return None
+
+    def is_answer_similar(self, user_answer, correct_answer, threshold=80):
+        similarity_score = fuzz.token_set_ratio(user_answer, correct_answer)
+        return similarity_score >= threshold
+    
+    def score_user_answer(self, correct_answer, max_attempts=2):
+        for i in range(max_attempts):
+            user_answer = input("Твой ответ: ").lower()
+            if self.is_answer_similar(user_answer, correct_answer):
+                print("Молодец! Твой ответ верный.\n")
+                return 1
+            print("Жаль, но твой ответ неправильный.\n") if i == 1 else print("Пока неверно. Попробуй еще раз.")
+        return 0
+    
+if __name__ == "__main__":
+    print("Привет! Предлагаю поиграть в викторину на английском языку. Отвечай на вопросы и зарабатывай очки. Количество вопросов ты определяешь самостоятельно.")
+    bot = Bot()
+    bot.run()
--- tests.py
+++ tests.py
@@ -1,9 +1,11 @@
 import unittest
 import requests
 from unittest.mock import patch
-from functions import *
+from bot import Bot
 
-class TestMakeApiCall(unittest.TestCase):
+bot = Bot()
+
+class TestBot(unittest.TestCase):
     # успешный вызов API
     def test_make_api_call_valid_input(self):
         with patch("requests.get") as mock_get:
@@ -11,17 +13,17 @@ class TestMakeApiCall(unittest.TestCase):
             mock_response.status_code = 200
             mock_get.return_value = mock_response
 
-            result = make_api_call(2)
+            result = Bot.make_api_call(bot, 2)
 
             mock_get.assert_called_once_with(
-                default_url,
+                bot.url,
                 params={'count': 2}
             )
             self.assertEqual(result, mock_response)
 
     # число вопросов для викторины не определено
     def test_make_api_call_invalid_input(self):
-        result = make_api_call(None)
+        result = Bot.make_api_call(bot, None)
         self.assertIsNone(result)
 
     # неуспешный вызов API
@@ -31,29 +33,28 @@ class TestMakeApiCall(unittest.TestCase):
             mock_response.status_code = 404
             mock_get.return_value = mock_response
 
-            result = make_api_call(3)
+            result = Bot.make_api_call(bot, 3)
 
             mock_get.assert_called_once_with(
-                default_url,
+                bot.url,
                 params={'count': 3}
             )
             self.assertIsNone(result)
 
-class TestsScoreUserAnswer(unittest.TestCase):
     # верный ответ
     def test_score_user_answer_right(self):
         with patch('builtins.input', return_value="USA"):
-            assert score_user_answer("the USA") == 1
+            assert Bot.score_user_answer(bot, "the USA") == 1
 
     # 2 неверных ответ
     def test_score_user_answer_wrong(self):
         with patch('builtins.input', side_effect=["Canada", "Mexico"]):
-            assert score_user_answer("the USA") == 0
+            assert Bot.score_user_answer(bot, "the USA") == 0
 
     # верная вторая попытка
     def test_score_user_answer_second_attempt(self):
         with patch('builtins.input', side_effect=["Canada", "USA"]):
-            assert score_user_answer("apple") == 0
+            assert Bot.score_user_answer(bot, "apple") == 0
 
 if __name__ == "__main__":
     unittest.main()
